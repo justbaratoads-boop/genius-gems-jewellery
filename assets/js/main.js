82ddf2908c0cc78eb4e8eddaa184b6c9823a377d
Kajal <justbarato.ads@gmail.com>
2026-07-20 22:12:12 +0530

    Configure live Google Sheet Apps Script Web App URL in main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@
 // --- BACKEND DATABASE CONFIGURATION ---
 // Paste your Google Sheet published Web App URL below to use Google Sheets as database.
 // Leave as "YOUR_GOOGLE_SCRIPT_WEB_APP_URL" to use Hostinger PHP MySQL by default.
-const GOOGLE_SCRIPT_URL = "YOUR_GOOGLE_SCRIPT_WEB_APP_URL"; 
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbz74IY0QY9TDplW1dfPHBcaHfp6RcsB-Tm8qk2IOVgEdNvHUuJ1vXapsn1MtzDqO6BZ/exec"; 
 
 const getApiUrl = (endpoint) => {
     if (typeof GOOGLE_SCRIPT_URL !== 'undefined' && GOOGLE_SCRIPT_URL !== "YOUR_GOOGLE_SCRIPT_WEB_APP_URL" && GOOGLE_SCRIPT_URL.trim() !== "") {
